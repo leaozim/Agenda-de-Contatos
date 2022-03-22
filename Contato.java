1d271772dfeab8c046d33512ecc4dfde43cef44d
Larissa Silva <larissa_silva@outlook.com>
2022-03-22 15:21:57 -0300

    Altera os arquivos .class

--- Contato.java
+++ Contato.java
@@ -1,3 +1,21 @@
-public class Contato extends Pessoa{
-      
+public class Contato extends Pessoa {
+    private int numero;
+
+    public Contato (){
+
+    }
+    public Contato(String NomeCompleto, int contato ){
+
+    }
+    public int setNumero(){
+        return numero;
+    }
+    public void getNumero(){
+        this.numero = numero;
+    }
+
+    public void NomeCompleto(){
+
+    }
+
 }
